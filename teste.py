47d50c8eb04cc023b1ae7a6ef743c9830410a133
arthur <142703193+arthvr9@users.noreply.github.com>
2024-10-18 15:57:19 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -7,7 +7,7 @@ def conectar():
         host='127.0.0.1',
         port='3306',
         user='root',
-        password='arthur301082',
+        password='aaaaaaaaaaa',
         database='form')
 
 def executar_consulta(conexao, consulta):
